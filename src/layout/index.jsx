5d03c0ca9f5028396a1d02088faa50b93e39c517
alli <alli@trip.com>
2021-12-31 16:08:02 +0800

    feat(): 页面布局

---
+++ src/layout/index.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+
+import { Layout, Menu } from 'antd'
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons'
+
+const { Header, Sider, Content } = Layout
+export default function Index(props) {
+  const [collapsed, setCollapsed] = useState(false)
+
+  const toggle = () => {
+    setCollapsed(!collapsed)
+  }
+  return (
+    <>
+      <Layout className="h-screen">
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: toggle,
+            })}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              minHeight: 280,
+            }}>
+            Content
+            <Outlet></Outlet>
+          </Content>
+        </Layout>
+      </Layout>
+    </>
+  )
+}
