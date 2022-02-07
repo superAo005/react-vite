d6424ea282f96c9a5f3a83b6e391a4468f8e164b
zhenwangYang <zhenwangyang@trip.com>
2022-02-07 17:30:59 +0800

    pref:优化

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -11,47 +11,47 @@ import {
 } from '@ant-design/icons'
 
 const { Header, Sider, Content } = Layout
-export default function Index(props) {
+import './index.less'
+function layout(props) {
   const [collapsed, setCollapsed] = useState(false)
 
   const toggle = () => {
     setCollapsed(!collapsed)
   }
   return (
-    <>
-      <Layout className="h-screen">
-        <Sider trigger={null} collapsible collapsed={collapsed}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1" icon={<UserOutlined />}>
-              nav 1
-            </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-              nav 2
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UploadOutlined />}>
-              nav 3
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }}>
-            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'trigger',
-              onClick: toggle,
-            })}
-          </Header>
-          <Content
-            className="site-layout-background"
-            style={{
-              margin: '24px 16px',
-              minHeight: 280,
-            }}>
-            Content
-            <Outlet></Outlet>
-          </Content>
-        </Layout>
+    <Layout className="h-screen">
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            nav 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            nav 2
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            nav 3
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: toggle,
+          })}
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: '24px 16px',
+            minHeight: 280,
+            padding: 24,
+          }}>
+          <Outlet></Outlet>
+        </Content>
       </Layout>
-    </>
+    </Layout>
   )
 }
+export default layout
