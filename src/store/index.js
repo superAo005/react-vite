c9f27a6ee4de0b3562ee678d7d845c7b61a0c0ee
zhenwangYang <zhenwangyang@trip.com>
2022-09-05 14:17:39 +0800

    feat:建立起一个基于webpack + react + typescript的标准化前端应用

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterSlice from '../features/counter/counterSlice';
-
-export default configureStore({
-  reducer: {
-    counter: counterSlice,
-  },
-});
