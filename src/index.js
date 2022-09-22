b2c4c74fbf8de9b349542265e405a101f9b01e89
zhenwangYang <zhenwangyang@trip.com>
2022-09-22 14:20:19 +0800

    feat:初始化

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import {persistor} from './redux/store';
-import {PersistGate} from 'redux-persist/lib/integration/react';
-import Login from '@pages/login/login';
-import Admin from '@pages/admin/admin';
-import reportWebVitals from './reportWebVitals';
-import store from './redux/store';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { persistor } from './redux/store'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import Login from '@pages/login/login'
+import Admin from '@pages/admin/admin'
+import reportWebVitals from './reportWebVitals'
+import store from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading='loading.....' persistor={persistor}>
-     <Router>
-      <Switch>
-        <Route path='/login' component={Login}></Route>
-        <Route path='/' component={Admin}></Route>
-      </Switch>
-    </Router>
+    <PersistGate loading="loading....." persistor={persistor}>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/" component={Admin}></Route>
+        </Switch>
+      </Router>
     </PersistGate>
   </Provider>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlPlugin = require('html-webpack-plugin');
-const {
-  CleanWebpackPlugin
-} = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   performance: {
@@ -17,79 +15,84 @@ module.exports = {
       '@src': path.resolve(__dirname, 'src'),
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@components': path.resolve(__dirname, 'src/components'),
-    }
+    },
   },
   devtool: 'inline-source-map', // 帮助发现错误
   entry: {
-    index: path.join(__dirname, 'src/index.js')
+    index: path.join(__dirname, 'src/index.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].[hash:7].js',
     assetModuleFilename: 'images/[name].[hash][ext][query]',
-    publicPath:'/' //这个是为了解决二级菜单刷新报错的问题，假如不加刷新后会去寻找当前路径的下打包后的css或者js，导致报错，所以要改设置为根路径
+    publicPath: '/', //这个是为了解决二级菜单刷新报错的问题，假如不加刷新后会去寻找当前路径的下打包后的css或者js，导致报错，所以要改设置为根路径
   },
   optimization: {
     splitChunks: {
       chunks: 'all',
       minSize: 60000,
       maxSize: 200000,
-    }
+    },
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.(js|jsx)$/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            plugins: [ // 按需引入antd
-              [
-                require.resolve('babel-plugin-import'),
-                {
-                  libraryName: 'antd', //暴露antd
-                  libraryDirectory: 'es',
-                  style: true
-                }
-              ]
-            ]
-          }
-        }],
-        exclude: /node_modules/
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                // 按需引入antd
+                [
+                  require.resolve('babel-plugin-import'),
+                  {
+                    libraryName: 'antd', //暴露antd
+                    libraryDirectory: 'es',
+                    style: true,
+                  },
+                ],
+              ],
+            },
+          },
+        ],
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|svg|jpg|gif)$/, // webpack5 已经弃用url-loader，file-loader
-        type: 'asset/resource'
-
+        type: 'asset/resource',
       },
       {
         test: /\.css$/,
         exclude: path.resolve(__dirname, 'node_modules'),
-        use: [{
+        use: [
+          {
             loader: MiniCssExtractPlugin.loader,
           },
           // {
           //   loader: 'style-loader'
           // },
           {
-            loader: 'css-loader'
+            loader: 'css-loader',
           },
-        ]
+        ],
       },
       {
         test: /\.less$/,
-        use: [ //webpack 5 自定义主题
+        use: [
+          //webpack 5 自定义主题
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
               esModule: false,
               // publicPath: './',
-            }
+            },
           },
           // {
           //   loader: 'style-loader'
           // },
           {
-            loader: 'css-loader'
+            loader: 'css-loader',
           },
           {
             loader: 'less-loader',
@@ -97,32 +100,41 @@ module.exports = {
               modifyVars: {
                 'primary-color': '#1DA57A',
               },
-              javascriptEnabled: true // 此项不能忘 
-            }
-          }
-        ]
+              javascriptEnabled: true, // 此项不能忘
+            },
+          },
+        ],
       },
-    ]
+    ],
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: path.join(__dirname, 'public'),
+    // contentBase
+    // static: path.join(__dirname, 'public/'),
+    static: {
+      directory: path.join(__dirname, 'public/'),
+    },
+    devMiddleware: {
+      publicPath: '/dist/',
+    },
+    static: './public',
     port: 8088, // 端口号
     inline: true,
-    hot: true,
-    open: true
+    // hotOnly
+    hot: 'only',
+    open: true,
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlPlugin({
       title: 'react-admin',
       template: path.resolve(__dirname, './public/index.html'),
-      filename: 'index.html'
+      filename: 'index.html',
     }),
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash].css',
       // chunkFilename: 'css/[id].css'
     }),
     new webpack.HotModuleReplacementPlugin(),
-  ]
+  ],
 }
