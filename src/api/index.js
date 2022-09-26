9a1c962fc75eb09a5079c3f6aa812d2e92b54ed5
zhenwangYang <zhenwangyang@trip.com>
2022-09-26 13:43:47 +0800

    feat:优化

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
-import request from './request';
+import request from './request'
 // import {message} from 'antd';
 // 获取天气
-export const getWeather = () => request('https://restapi.amap.com/v3/weather/weatherInfo', {
-  city: 510100,
-  key: '7643af4d54f441a88a37a3307be2fff0'
-});
+export const getWeather = () =>
+  request('https://restapi.amap.com/v3/weather/weatherInfo', {
+    city: 510100,
+    key: '7643af4d54f441a88a37a3307be2fff0',
+  })
 // 登录
-export const reqLogin =  (username, password) => request('/login', {username, password}, 'POST');
+export const reqLogin = (username, password) => request('/login', { username, password }, 'POST')
 // 获取菜单
-export const reqGetMenu = () => request('/getmenu');
-
+export const reqGetMenu = () => request('/getmenu')
